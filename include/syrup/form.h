7c8bcde543340c0a7fc15ea36db792d49a1dd85e
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-05-18 01:16:04 +0200

    adds styling

--- include/syrup/form.h
+++ include/syrup/form.h
@@ -8,13 +8,15 @@ extern "C" {
 #endif
 
 typedef struct sy_term_style_t {
-    
+
 } sy_term_style_t;
 
-size_t sy_term_form_list(const char *msg, const char **choices, size_t size);
-bool sy_term_form_confirm(const char *msg, bool clear);
-char *sy_term_form_prompt(const char *msg);
-char *sy_term_form_password(const char *msg, const char *sub);
+size_t sy_term_form_list(sy_term_style_t *style, const char *msg,
+                         const char **choices, size_t size);
+bool sy_term_form_confirm(sy_term_style_t *style, const char *msg, bool clear);
+char *sy_term_form_prompt(sy_term_style_t *style, const char *msg);
+char *sy_term_form_password(sy_term_style_t *style, const char *msg,
+                            const char *sub);
 
 #ifdef __cplusplus
 }
